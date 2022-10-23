ddbf35cfc55966fb6922d6af1334b935e829d016
ViditRawat <vidit.r@cymplstudios.com>
2022-10-23 13:55:49 +0530

    Bind item name, count and opacity

--- Source/UltimateShooter/Item.h
+++ Source/UltimateShooter/Item.h
@@ -6,6 +6,19 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+
+UENUM(BlueprintType)
+enum class EItemRarity : uint8
+{
+	EIR_Damage UMETA(DisplayName = "Damaged"),
+	EIR_Common UMETA(DisplayName = "Common"),
+	EIR_Uncommon UMETA(DisplayName = "UnCommon"),
+	EIR_Rare UMETA(DisplayName = "Rare"),
+	EIR_Legendary UMETA(DisplayName = "Legendary"),
+
+	EIR_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
 UCLASS()
 class ULTIMATESHOOTER_API AItem : public AActor
 {
@@ -37,6 +50,10 @@ protected:
 		UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex);
 
+	//set stars based on rairty
+	void SetActiveStars();
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -58,6 +75,21 @@ private:
 	//Enables item tracing when overlaped
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	class USphereComponent* AreaSphere;
+
+	// The name which appears on the pickup widget 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	FString ItemName;
+
+	//Item count (ammo)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	int32 ItemCount;
+
+	//determines determines number of star in pickup widget
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	EItemRarity ItemRarity;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	TArray<bool> ActiveStars;
 
 	
 
